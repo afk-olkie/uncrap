795b3edd6b91a746449275aded4979c53e123e29
Adam FK <adam.k@jcaelectronics.ca>
2017-11-24 11:15:31 -0600

    style: using vscode auto format

--- src/FoundFunction.ts
+++ src/FoundFunction.ts
@@ -4,61 +4,61 @@
 */
 
 export class FoundFunction {
-  before : string = null;
-  comments : string = null;
-  returnType : string = null;
-  functionName : string = null;
-  parameters : string = null;
+  before: string = null;
+  comments: string = null;
+  returnType: string = null;
+  functionName: string = null;
+  parameters: string = null;
   declared = false;
 
-  hasParameters() : boolean {
+  hasParameters(): boolean {
     let has = this.parameters && this.parameters != "void";
     return has;
   }
 
-  
-  hasNonVoidReturnType() : boolean {
+
+  hasNonVoidReturnType(): boolean {
     let result = this.returnType.trim().match(/^(extern\s+)?void/) == null;
     return result;
   }
 
 
-  getParametersDeclArray(options?: {convertVariadic? : boolean}) : string[] {
+  getParametersDeclArray(options?: { convertVariadic?: boolean }): string[] {
     options = options || {};
-    let params : string[] = [];
+    let params: string[] = [];
 
-    if (this.hasParameters()){
+    if (this.hasParameters()) {
       params = this.getParameters(options).split(/\s*,\s*/);
     }
 
     return params;
   }
 
-  hasVariadicArgument() : boolean {
-    let result : boolean;
+  hasVariadicArgument(): boolean {
+    let result: boolean;
     result = this.parameters.match(/[.][.][.]/) != null;
     return result;
   }
 
-  getParameterNames(options?: {convertVariadic? : boolean}) : string[] {
+  getParameterNames(options?: { convertVariadic?: boolean }): string[] {
     options = options || {};
-    let result : string[] = [];
+    let result: string[] = [];
 
-    let params : string[] = this.getParametersDeclArray(options);
+    let params: string[] = this.getParametersDeclArray(options);
 
-    if(params.length == 0){
+    if (params.length == 0) {
       return result;
     }
 
-    for(let paramDecl of params) {
+    for (let paramDecl of params) {
       paramDecl = paramDecl.trim();
 
       //skip over variadic parameters "..." and "void".
-      if(paramDecl.match(/^([.][.][.]|void)$/)) {
+      if (paramDecl.match(/^([.][.][.]|void)$/)) {
         continue;
       }
-      let match = paramDecl.trim().match(/\b(\w+)(?=$|\[.*)/); 
-      if(!match){
+      let match = paramDecl.trim().match(/\b(\w+)(?=$|\[.*)/);
+      if (!match) {
         throw "didn't match!";
       }
       let name = match[1];
@@ -68,61 +68,61 @@ export class FoundFunction {
     return result;
   }
 
-  getParameterNamesString(options?: {convertVariadic? : boolean}) : string {
+  getParameterNamesString(options?: { convertVariadic?: boolean }): string {
     options = options || {};
-    let params : string[] = this.getParameterNames(options);
+    let params: string[] = this.getParameterNames(options);
 
     let result = params.join(", ");
     return result;
-  }  
+  }
 
-  getParameters(options?: {convertVariadic? : boolean}) {
+  getParameters(options?: { convertVariadic?: boolean }) {
     options = options || {};
     let result = this.parameters;
 
-    if(options.convertVariadic){
+    if (options.convertVariadic) {
       result = convertVariardicToVaList(result);
     }
 
     return result;
   }
 
-  getParametersPrepended(toPrepend : string, options?: {convertVariadic? : boolean}) {
+  getParametersPrepended(toPrepend: string, options?: { convertVariadic?: boolean }) {
     options = options || {};
     let result = toPrepend;
 
-    if( this.hasParameters() ){
+    if (this.hasParameters()) {
       result += ", " + this.parameters;
     }
 
-    if(options.convertVariadic){
+    if (options.convertVariadic) {
       result = convertVariardicToVaList(result);
     }
 
     return result;
   }
 
-  getSignatureMismatchString(foundFunction){
+  getSignatureMismatchString(foundFunction) {
     //NOTE: not smart enough for 'extern' and stuff like that
-    if(foundFunction.returnType != this.returnType){
+    if (foundFunction.returnType != this.returnType) {
       return "return types do not match!";
     }
 
     //count parameters
     //NOTE: no support for strings with "," in it
-    if((foundFunction.parameters.match(/.+?(,|$)/g) || []).length != (this.parameters.match(/.+?(,|$)/g) || []).length){
+    if ((foundFunction.parameters.match(/.+?(,|$)/g) || []).length != (this.parameters.match(/.+?(,|$)/g) || []).length) {
       return "parameter count does not match!";
     }
     return "";
   };
 
-  toString(){
-    return "FoundFunction{ returnType='" + this.returnType + "', functionName='"+this.functionName+"', parameters='"+this.parameters+"', declared='"+this.declared+"' }";
+  toString() {
+    return "FoundFunction{ returnType='" + this.returnType + "', functionName='" + this.functionName + "', parameters='" + this.parameters + "', declared='" + this.declared + "' }";
   };
 }
 
-  
-function convertVariardicToVaList(paramDecls : string) : string {
+
+function convertVariardicToVaList(paramDecls: string): string {
   let result = paramDecls.replace(/[.][.][.]/, "va_list args");
   return result;
 }
--- ui/c-proto-gen/processor.ts
+++ ui/c-proto-gen/processor.ts
@@ -5,23 +5,23 @@
 // import { FoundFunction } from "../../src/UncrapCore";
 
 class Target {
-  sortBy1 : string;
-  sortBy2 : string;
-  sortBy3 : string;
-  includeComments : boolean;
-  input : string;
-  ifndefName : string;
-  output : string;
-  warnings : string;
-  log : string;
+  sortBy1: string;
+  sortBy2: string;
+  sortBy3: string;
+  includeComments: boolean;
+  input: string;
+  ifndefName: string;
+  output: string;
+  warnings: string;
+  log: string;
 }
 
-function processTarget(tgt : Target) : void {
-  var foundFunctionsArray : FoundFunction[] = [];
+function processTarget(tgt: Target): void {
+  var foundFunctionsArray: FoundFunction[] = [];
   var declaredCount = 0;
   var declaredMap = {};
   var definedMap = {};
-  var definedArray : FoundFunction[] = [];   //from c input box
+  var definedArray: FoundFunction[] = [];   //from c input box
 
 
   tgt.output = "";
@@ -34,44 +34,44 @@ function processTarget(tgt : Target) : void {
 
   for (const foundFunction of foundFunctionsArray) {
     tgt.log += "found function: " + foundFunction + "\n";
-    if(!foundFunction.declared){
+    if (!foundFunction.declared) {
       //add to defined
-      if(definedMap[foundFunction.functionName]){
+      if (definedMap[foundFunction.functionName]) {
         tgt.warnings += "MULTIPLE definitions found for '" + foundFunction.functionName + "'\n";
         tgt.warnings += "  > previous definition: " + definedMap[foundFunction.functionName] + "\n";
         tgt.warnings += "  >    other definition: " + foundFunction + "\n";
-      }else{
+      } else {
         definedArray.push(foundFunction);
       }
       definedMap[foundFunction.functionName] = foundFunction;
-    }else{
+    } else {
       //add to declared
       tgt.log += " > detected as declared\n";
-      if(declaredMap[foundFunction.functionName]){
+      if (declaredMap[foundFunction.functionName]) {
         tgt.warnings += "MULTIPLE declarations found for '" + foundFunction.functionName + "'\n";
         tgt.warnings += "  > previous declaration: " + declaredMap[foundFunction.functionName] + "\n";
         tgt.warnings += "  >    other declaration: " + foundFunction + "\n";
-      }else{
+      } else {
         declaredCount++;
       }
       declaredMap[foundFunction.functionName] = foundFunction;
     }
   }
 
-  
-  tgt.log += "Function stats: defined=" + definedArray.length + ", declared="+declaredCount+"\n";
-  if(definedArray.length === 0){
+
+  tgt.log += "Function stats: defined=" + definedArray.length + ", declared=" + declaredCount + "\n";
+  if (definedArray.length === 0) {
     tgt.warnings += "No defined functions found!\n";
   }
 
   tgt.log += "------------------------------------------------------\n";
-  if(tgt.sortBy1 || tgt.sortBy2 || tgt.sortBy3){
-      tgt.log += "sorting defined functions based on fields: " + tgt.sortBy1 + ", " + tgt.sortBy2 + ", " + tgt.sortBy3+ "\n";
-      definedArray = definedArray.sort(function(a,b){
-        var aStr = getSortString([tgt.sortBy1, tgt.sortBy2, tgt.sortBy3], a);
-        var bStr = getSortString([tgt.sortBy1, tgt.sortBy2, tgt.sortBy3], b);
-        return aStr.localeCompare(bStr);
-      });
+  if (tgt.sortBy1 || tgt.sortBy2 || tgt.sortBy3) {
+    tgt.log += "sorting defined functions based on fields: " + tgt.sortBy1 + ", " + tgt.sortBy2 + ", " + tgt.sortBy3 + "\n";
+    definedArray = definedArray.sort(function (a, b) {
+      var aStr = getSortString([tgt.sortBy1, tgt.sortBy2, tgt.sortBy3], a);
+      var bStr = getSortString([tgt.sortBy1, tgt.sortBy2, tgt.sortBy3], b);
+      return aStr.localeCompare(bStr);
+    });
   }
 
   tgt.log += "------------------------------------------------------\n";
@@ -79,15 +79,15 @@ function processTarget(tgt : Target) : void {
 
   for (const foundFunction of definedArray) {
     var declaration = declaredMap[foundFunction.functionName];
-    if(declaration === undefined){
-      if(tgt.includeComments && foundFunction.comments){
+    if (declaration === undefined) {
+      if (tgt.includeComments && foundFunction.comments) {
         tgt.output += foundFunction.comments.trim() + "\n";
       }
       tgt.output += foundFunction.returnType + " " + foundFunction.functionName + "(" + foundFunction.parameters + ");\n";
-    }else{
+    } else {
       tgt.log += "not rendering function '" + foundFunction.functionName + "' because it was already declared.\n";
-      var mismatchString = foundFunction.getSignatureMismatchString( declaration );
-      if(mismatchString !== ""){
+      var mismatchString = foundFunction.getSignatureMismatchString(declaration);
+      if (mismatchString !== "") {
         tgt.warnings += "INCOMPATIBLE SIGNATURES FOUND between declaration and definition because " + mismatchString + "\n";
         tgt.warnings += " > declaration: " + declaration + "\n";
         tgt.warnings += " >  definition: " + foundFunction + "\n";
@@ -96,7 +96,7 @@ function processTarget(tgt : Target) : void {
     //check that signature matches based on return type and number of parameters
   }
 
-  if(tgt.ifndefName){
+  if (tgt.ifndefName) {
     //TODO update to use backtick templates
     tgt.output = "#ifndef " + tgt.ifndefName + "_H\n#define " + tgt.ifndefName + "_H\n\n" + tgt.output + "\n#endif /* " + tgt.ifndefName + "_H */\n";
   }
@@ -105,11 +105,11 @@ function processTarget(tgt : Target) : void {
 
 
 //------------------------------------------------------------------------------
-function getSortString(sortByArray, object){
+function getSortString(sortByArray, object) {
   var result = "";
 
   for (const sortByProperty of sortByArray) {
-    if(sortByProperty){
+    if (sortByProperty) {
       result += object[sortByProperty]
     }
   }
@@ -117,9 +117,9 @@ function getSortString(sortByArray, object){
   return result;
 }
 
-function arraysIntersect(array1, array2){
+function arraysIntersect(array1, array2) {
   var result = false;
-  for(var i=0; !result && i < array2.length; i++){
+  for (var i = 0; !result && i < array2.length; i++) {
     result = array1.indexOf(array2[i]) !== -1;
   }
   return result;
